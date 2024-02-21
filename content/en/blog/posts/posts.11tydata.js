ac4722abc2c162b649a113afe4f9c4c6f2da40c3
Sylvain Lafay <sylvain.lafay@ign.fr>
2024-02-21 17:30:24 +0100

    retire les pages en anglais sauf accueil et à propos

--- content/en/blog/posts/posts.11tydata.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    tags: [
-        "posts"
-    ],
-    layout: "layouts/post.njk",
-    permalink: function (data) {
-        return `/${data.lang}/blog/${data.page.fileSlug}/`;
-    }
-};
