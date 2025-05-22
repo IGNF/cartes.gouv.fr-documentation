e42f5f08b6c1e96cd07988f17a9346b8ffee59c3
Clement Brousseau <clement.brousseau@ign.fr>
2025-05-22 14:09:19 +0200

    feat: ajout de la pagination et du filtre par types de guides #111 #130

--- public/js/search-results.js
+++ public/js/search-results.js
@@ -5,7 +5,7 @@ const RESULT_COUNT_SELECTOR = "#result-count";
 const SEARCH_RESULTS_SELECTOR = "#search-results";
 const SEARCH_FILTER_TAGS_SELECTOR = "#search-filter-tags";
 
-const FULL_WIDTH_COL_CLASS = "fr-col-8";
+const FULL_WIDTH_COL_CLASS = "fr-col-12 fr-col-lg-8";
 
 class PageFinder {
     constructor(pagefind) {
@@ -43,23 +43,27 @@ class PageFinder {
 
         // nouveaux résultats donc on les nettoie et on les affiche
         this.start = 0;
-        let paginatedResults = await Promise.all(this.searchResults.slice(this.start, this.start + RESULTS_PER_PAGE).map((r) => r.data()));
-
-        await this.populateSearchResults(paginatedResults, true);
-
-        // chargement des éléments de plus de RESULTS_PER_PAGE au scroll
-        window.addEventListener("scroll", async () => {
-            if (this._bottomIsReached()) {
-                this.start += RESULTS_PER_PAGE;
-                paginatedResults = await Promise.all(this.searchResults.slice(this.start, this.start + RESULTS_PER_PAGE).map((r) => r.data()));
-                await this.populateSearchResults(paginatedResults);
-            }
-        });
+        await this.goToPage(0);
 
         return {
             results: search.results,
             filters,
         };
+    }
+
+    async goToPage(pageIndex) {
+        this.start = pageIndex * RESULTS_PER_PAGE;
+
+        const paginatedResults = await Promise.all(this.searchResults.slice(this.start, this.start + RESULTS_PER_PAGE).map((r) => r.data()));
+
+        await this.populateSearchResults(paginatedResults, true);
+
+        const totalPages = Math.ceil(this.searchResults.length / RESULTS_PER_PAGE);
+        this.renderPagination(pageIndex, totalPages);
+
+        requestAnimationFrame(() => {
+            document.documentElement.scrollIntoView({ behavior: "smooth" });
+        });
     }
 
     /**
@@ -88,55 +92,69 @@ class PageFinder {
     populateFilters(filters, initHashFilters) {
         this.filterTagsEl.innerHTML = "";
 
-        Object.entries(filters).map(([filterType, subFilter]) => {
-            let div = document.createElement("div");
-            div.className = "fr-grid-row fr-grid-row--middle fr-my-2v";
+        const specialGuideTags = ["Utilisateur", "Développeur", "Producteur"];
 
-            let divTitle = document.createElement("div");
-            divTitle.className = "fr-col-12 fr-mb-2v";
-            divTitle.textContent = `${filterType.charAt(0).toUpperCase() + filterType.slice(1)} : `;
+        Object.entries(filters).forEach(([filterType, subFilter]) => {
+            const tagEntries = Object.entries(subFilter);
 
-            let divTags = document.createElement("div");
-            divTags.className = "fr-col-8";
+            const specialTags = tagEntries.filter(([tag]) => specialGuideTags.includes(tag));
+            const otherTags = tagEntries.filter(([tag]) => !specialGuideTags.includes(tag));
 
-            let ulTags = document.createElement("ul");
-            ulTags.className = "fr-tags-group";
-            divTags.appendChild(ulTags);
+            if (specialTags.length > 0) {
+                this.renderFilterBlock(filterType, specialTags, initHashFilters, "Types de guides :");
+            }
 
-            div.appendChild(divTitle);
-            div.appendChild(divTags);
+            if (otherTags.length > 0) {
+                const title = `${filterType.charAt(0).toUpperCase() + filterType.slice(1)} :`;
+                this.renderFilterBlock(filterType, otherTags, initHashFilters, title);
+            }
+        });
+    }
 
-            Object.entries(subFilter).map(([filterKeyword, count]) => {
-                const button = document.createElement("button");
-                button.className = "fr-tag";
-                button.ariaPressed = initHashFilters[filterType]?.includes(filterKeyword) ? "true" : "false";
-                button.textContent = `${filterKeyword} (${count})`;
+    renderFilterBlock(filterType, tagEntries, initHashFilters, title) {
+        const div = document.createElement("div");
+        div.className = "fr-grid-row fr-grid-row--middle fr-my-2v";
 
-                button.addEventListener("click", (e) => {
-                    e.preventDefault();
-                    const button = e.currentTarget;
+        const divTitle = document.createElement("div");
+        divTitle.className = "fr-col-12 fr-mb-2v";
+        divTitle.textContent = title;
 
-                    const hashFilters = this.getFiltersFromHash();
+        const divTags = document.createElement("div");
+        divTags.className = "fr-col-12 fr-col-lg-8";
 
-                    if (button.ariaPressed === "true") {
-                        // on enlève
-                        hashFilters[filterType] = hashFilters[filterType]?.filter((f) => f !== filterKeyword);
-                        if (hashFilters[filterType]?.length === 0) {
-                            delete hashFilters[filterType];
-                        }
-                    } else {
-                        // on ajoute
-                        hashFilters[filterType] = Array.from(new Set([...(hashFilters[filterType] ?? []), filterKeyword]));
+        const ulTags = document.createElement("ul");
+        ulTags.className = "fr-tags-group";
+        divTags.appendChild(ulTags);
+
+        tagEntries.forEach(([filterKeyword, count]) => {
+            const button = document.createElement("button");
+            button.className = "fr-tag";
+            button.ariaPressed = initHashFilters[filterType]?.includes(filterKeyword) ? "true" : "false";
+            button.textContent = `${filterKeyword} (${count})`;
+
+            button.addEventListener("click", (e) => {
+                e.preventDefault();
+                const button = e.currentTarget;
+                const hashFilters = this.getFiltersFromHash();
+
+                if (button.ariaPressed === "true") {
+                    hashFilters[filterType] = hashFilters[filterType]?.filter((f) => f !== filterKeyword);
+                    if (!hashFilters[filterType]?.length) {
+                        delete hashFilters[filterType];
                     }
+                } else {
+                    hashFilters[filterType] = Array.from(new Set([...(hashFilters[filterType] ?? []), filterKeyword]));
+                }
 
-                    window.location.hash = Object.keys(hashFilters).length === 0 ? "" : encodeURIComponent(JSON.stringify(hashFilters));
-                });
-
-                ulTags.appendChild(button);
+                window.location.hash = Object.keys(hashFilters).length === 0 ? "" : encodeURIComponent(JSON.stringify(hashFilters));
             });
 
-            this.filterTagsEl.appendChild(div);
+            ulTags.appendChild(button);
         });
+
+        div.appendChild(divTitle);
+        div.appendChild(divTags);
+        this.filterTagsEl.appendChild(div);
     }
 
     getFiltersFromHash() {
@@ -195,6 +213,61 @@ class PageFinder {
         const suffix = endsWithDot ? "" : " ...";
 
         return `${prefix}${excerpt.trim()}${suffix}`;
+    }
+
+    renderPagination(currentPage, totalPages) {
+        if (this.searchResults.length <= RESULTS_PER_PAGE) {
+            document.querySelector("#pagination-container").innerHTML = "";
+            return;
+        }
+
+        const container = document.querySelector("#pagination-container");
+        container.innerHTML = "";
+
+        const ul = document.createElement("ul");
+        ul.className = "fr-pagination__list";
+
+        const createPageLink = (page, label, isDisabled = false, isCurrent = false) => {
+            const li = document.createElement("li");
+            const a = document.createElement("a");
+            a.className = "fr-pagination__link";
+            if (label === "Page précédente") a.classList.add("fr-pagination__link--prev", "fr-pagination__link--lg-label");
+            if (label === "Page suivante") a.classList.add("fr-pagination__link--next", "fr-pagination__link--lg-label");
+
+            a.setAttribute("role", "link");
+            if (isDisabled) a.setAttribute("aria-disabled", "true");
+            else {
+                a.href = "#";
+                a.addEventListener("click", (e) => {
+                    e.preventDefault();
+                    this.goToPage(page);
+                });
+            }
+
+            if (isCurrent) {
+                a.setAttribute("aria-current", "page");
+            }
+
+            a.textContent = label || page;
+            li.appendChild(a);
+            return li;
+        };
+
+        ul.appendChild(createPageLink(currentPage - 1, "Page précédente", currentPage === 0));
+
+        for (let i = 0; i < totalPages; i++) {
+            if (Math.abs(i - currentPage) <= 2 || i === 0 || i === totalPages - 1) {
+                ul.appendChild(createPageLink(i, String(i + 1), false, i === currentPage));
+            } else if ((i === currentPage - 3 && currentPage > 3) || (i === currentPage + 3 && currentPage < totalPages - 4)) {
+                const li = document.createElement("li");
+                li.innerHTML = `<span class="fr-pagination__link">…</span>`;
+                ul.appendChild(li);
+            }
+        }
+
+        ul.appendChild(createPageLink(currentPage + 1, "Page suivante", currentPage >= totalPages - 1));
+
+        container.appendChild(ul);
     }
 
     _getCardHtml(title, excerpt, url, filters = []) {
