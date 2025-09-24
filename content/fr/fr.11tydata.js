b60b536f04770f3b8cfcfee6ad27f085ba70bcbc
Skander18 <Skander18@example.com>
2025-09-24 09:24:50 +0200

    Create publication-tmsv

--- content/fr/fr.11tydata.js
+++ content/fr/fr.11tydata.js
@@ -11,6 +11,9 @@ module.exports = {
         public: {
             wmsv: "https://data.geopf.fr/wms-v",
         },
+        public: {
+            tmsv: "https://data.geopf.fr/vector-tms",
+        },
     },
     permalink: function (data) {
         if (data.slugOverride) {
