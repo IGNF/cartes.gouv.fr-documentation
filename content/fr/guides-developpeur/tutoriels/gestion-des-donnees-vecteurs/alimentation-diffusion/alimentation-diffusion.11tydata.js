ddd3b0e174eee20307e69c6fd3740b88f34e355e
Skander Mokni <skander.mokni@ign.fr>
2025-10-02 10:05:28 +0200

    Correct vector referencing

---
+++ content/fr/guides-developpeur/tutoriels/gestion-des-donnees-vecteurs/alimentation-diffusion/alimentation-diffusion.11tydata.js
@@ -0,0 +1,8 @@
+module.exports = {
+    segments: [
+        {
+            url: "/guides-developpeur/tutoriels/gestion-des-donnees-vecteurs/alimentation-diffusion",
+            title: "Alimentation et diffusion simple",
+        },
+    ],
+};
