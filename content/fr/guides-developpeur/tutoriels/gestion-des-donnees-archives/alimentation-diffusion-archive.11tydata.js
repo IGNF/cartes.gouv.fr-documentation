76be983960293416fa137382c00934db9f01602c
Skander Mokni <skander.mokni@ign.fr>
2025-10-10 15:11:05 +0200

    Create file livraison archive

--- content/fr/guides-developpeur/tutoriels/gestion-des-donnees-archives/alimentation-diffusion-archive.11tydata.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    segments: [
-        {
-            url: "/guides-developpeur/tutoriels/gestion-des-donnees-archives",
-            title: "Alimentation et diffusion simple d'archives",
-        },
-    ],
-};
---
+++ content/fr/guides-developpeur/tutoriels/gestion-des-donnees-archives/alimentation-diffusion-archive/alimentation-diffusion-archive.11tydata.js
@@ -0,0 +1,8 @@
+module.exports = {
+    segments: [
+        {
+            url: "/guides-developpeur/tutoriels/gestion-des-donnees-archives/alimentation-diffusion-archive",
+            title: "Alimentation et diffusion simple",
+        },
+    ],
+};
