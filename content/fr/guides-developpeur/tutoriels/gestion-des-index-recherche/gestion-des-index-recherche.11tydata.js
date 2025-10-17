06c028a129be5158bd93b76de2aa844f9da5d3e7
Skander Mokni <skander.mokni@ign.fr>
2025-10-17 11:56:42 +0200

    Create file creation and index standard

---
+++ content/fr/guides-developpeur/tutoriels/gestion-des-index-recherche/gestion-des-index-recherche.11tydata.js
@@ -0,0 +1,8 @@
+module.exports = {
+    segments: [
+        {
+            url: "/guides-developpeur/tutoriels/gestion-des-index-recherche",
+            title: "Index de recherche",
+        },
+    ],
+};
