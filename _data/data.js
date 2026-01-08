33ffbe8817328e07bc1732041a0fbd2a3861fe54
Clement Brousseau <clement.brousseau@ign.fr>
2026-01-08 09:23:10 +0100

    feat: correction ui footer #248

--- _data/data.js
+++ _data/data.js
@@ -1,3 +1,3 @@
 module.exports = {
-    footer_content_description: `Cartes.gouv.fr est développé par l’Institut national de l’information géographique et forestière (IGN) et ses partenaires. Le site s’appuie sur la Géoplateforme, la nouvelle infrastructure publique, ouverte et collaborative des données géographiques.`,
+    footer_content_description: `Cartes.gouv.fr est le service public des cartes et données du territoire français. Porté par l’IGN et ses partenaires, il offre à tous un accès à la référence de la cartographie publique et permet à chacun de créer, d’héberger et de publier ses propres données et représentations.`,
 };
