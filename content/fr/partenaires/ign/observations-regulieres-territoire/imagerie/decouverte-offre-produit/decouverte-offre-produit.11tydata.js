eea705524608d6f843b3d36bea6e4a7378e7eebe
Clement Brousseau <clement.brousseau@ign.fr>
2026-02-12 11:14:34 +0100

    fix: exemple navigation tertiaire

--- content/fr/partenaires/ign/observations-regulieres-territoire/imagerie/decouverte-offre-produit/decouverte-offre-produit.11tydata.js
+++ content/fr/partenaires/ign/observations-regulieres-territoire/imagerie/decouverte-offre-produit/decouverte-offre-produit.11tydata.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: "Découverte de l’offre produit",
     eleventyNavigation: {
-        sidemenuActive: "/fr/partenaires/ign/observations-regulieres-territoire/imagerie/decouverte-imagerie",
+        sidemenuActive: "/fr/partenaires/ign/observations-regulieres-territoire/imagerie/decouverte-offre-produit",
     },
     eleventyExcludeFromCollections: true,
 };
