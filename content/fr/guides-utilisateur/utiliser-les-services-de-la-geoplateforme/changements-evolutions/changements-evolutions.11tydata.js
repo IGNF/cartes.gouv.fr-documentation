74cc908c3ea9a2fe5b56f8d9e92997467d507990
Skander Mokni <skander.mokni@ign.fr>
2026-03-06 10:40:45 +0100

    small fixes

--- content/fr/guides-utilisateur/utiliser-les-services-de-la-geoplateforme/changements-evolutions/changements-evolutions.11tydata.js
+++ content/fr/guides-utilisateur/utiliser-les-services-de-la-geoplateforme/changements-evolutions/changements-evolutions.11tydata.js
@@ -2,7 +2,7 @@ module.exports = {
     segments: [
         {
             url: "/guides-utilisateur/utiliser-les-services-de-la-geoplateforme/changements-evolutions",
-            title: "Changements & évolutions",
+            title: "Changements & évolutions de la Géoplateforme",
         },
     ],
 };
