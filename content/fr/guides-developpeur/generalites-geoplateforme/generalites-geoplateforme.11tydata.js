3cabf90ca572e069fdd433a14453dc51ee583021
Skander Mokni <skander.mokni@ign.fr>
2026-07-10 10:31:58 +0200

    rappatriement-generalites-geoplateforme&geoplateforme-en-bref

---
+++ content/fr/guides-developpeur/generalites-geoplateforme/generalites-geoplateforme.11tydata.js
@@ -0,0 +1,12 @@
+module.exports = {
+    tags: ["Géoplateforme", "Généralités"],
+    segments: [
+        {
+            url: "/guides-developpeur/generalites-geoplateforme/",
+            title: "Généralités Géoplateforme",
+        },
+    ],
+    eleventyNavigation: {
+        parent: "Généralités Géoplateforme",
+    },
+};
